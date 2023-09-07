b87713995291be3bafba4ec1714a2d17550ee4ab
yeryunJung <2bites@naver.com>
2023-09-07 14:16:39 +0900

    Init: cra로 초기 세팅 - pwa 적용해서 재세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
-    <RecoilRoot>
-      <div className="App" />;
-    </RecoilRoot>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
