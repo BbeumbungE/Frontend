b6dcf0317056d1b0175f71259cac6df50b09c185
yeryunJung <2bites@naver.com>
2023-09-08 17:37:33 +0900

    Feat: 라우터 PATH 설정 [FRONT-162] Resolves: issue #162

---
+++ client/src/pages/RankingPage.tsx
@@ -0,0 +1,9 @@
+function RankingPage() {
+  return (
+    <div>
+      <h1>RankingPage</h1>
+    </div>
+  );
+}
+
+export default RankingPage;
---
+++ client/src/pages/TopicDrawingPage.tsx
@@ -0,0 +1,9 @@
+function TopicDrawingPage() {
+  return (
+    <div>
+      <h1>TopicDrawingPage</h1>
+    </div>
+  );
+}
+
+export default TopicDrawingPage;
