c462da05530180b019d23eb8442840b9b2164327
sidsidsidsids <sid98816@gmail.com>
2023-09-08 17:48:13 +0900

    Feat: Atoms 일부 구현 [FRONT-33] - BlurBox Div - 감정표현 이모지 - 나가기 버튼 구성 요소 - 닉네임 입력 구성 요소 Related to: issue #33

---
+++ client/src/components/atoms/ExitText.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import theme from '../../style/theme';
+
+interface ExitTextProps {
+  color: string;
+}
+
+const ExitSpan = styled.span<{ txColor: string }>`
+  font-size: 40px;
+  font-family: 'TmoneyRoundWindExtraBold';
+  color: ${(props) => props.txColor};
+  cursor: pointer;
+`;
+
+function SpanColor({ color }: ExitTextProps) {
+  let txColor = theme.colors.darkGray;
+
+  switch (color) {
+    case 'dark':
+      txColor = theme.colors.darkGray;
+      break;
+    case 'light':
+      txColor = `#FFFFFF`;
+      break;
+    default:
+      txColor = theme.colors.darkGray;
+  }
+
+  return txColor; // 색상을 반환
+}
+
+const ExitText = ({ color }: ExitTextProps) => {
+  const txColor = SpanColor({ color }); // SpanColor 함수 호출
+  return <ExitSpan txColor={txColor}>나가기</ExitSpan>;
+};
+
+export default ExitText;
