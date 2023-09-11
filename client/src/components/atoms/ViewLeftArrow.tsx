2b9120966503497256636dc5302f06cc18fe0bb2
sidsidsidsids <sid98816@gmail.com>
2023-09-11 17:58:50 +0900

    Feat: 페이지 좌우 버튼 구현 [FRONT-169] - 두 버튼을 담고 포지션이 absolute인 div로 구현 Related to: issue #169

--- client/src/components/atoms/ViewLeftArrow.tsx
+++ client/src/components/atoms/ViewLeftArrow.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import styled from 'styled-components';
 import { ReactComponent as ViewLeftArrowIcon } from '../../assets/image/etc/ViewLeftArrow.svg';
 
+interface ViewLeftArrowProps {
+  onClick: React.MouseEventHandler<SVGSVGElement>;
+}
+
 const StyledViewLeftArrow = styled(ViewLeftArrowIcon)`
   cursor: pointer;
+  margin: 5px;
 `;
 
-const ViewLeftArrow = () => {
-  return <StyledViewLeftArrow />;
+const ViewLeftArrow = ({ onClick }: ViewLeftArrowProps) => {
+  return <StyledViewLeftArrow onClick={onClick} />;
 };
 
 export default ViewLeftArrow;
