b230deed451e8f3256744193e30cd27b049e3c4a
yeryunJung <2bites@naver.com>
2023-09-11 16:57:35 +0900

    Feat: UserRupee 및 LevelBtn 구현 [FRONT-161] - UserRupee의 리코일 atom 생성 - LevelBtn 은 레벨을 props로 전달 Related to: issues #161, #171

--- client/src/pages/MainMenuPage.tsx
+++ client/src/pages/MainMenuPage.tsx
@@ -2,12 +2,16 @@ import Button from '../components/atoms/Button';
 import Menu from '../components/atoms/Menu';
 import WhiteModal from '../components/atoms/WhiteModal';
 import ProfileBtn from '../components/atoms/ProfileBtn';
+import UserRupee from '../components/atoms/UserRupee';
+import LevelBtn from '../components/atoms/LevelBtn';
 
 function MainMenuPage() {
   return (
     <div>
       <h1>Home Page</h1>
       <ProfileBtn />
+      <UserRupee />
+      <LevelBtn level={1} />
       <Button buttonText="salmon" color="salmon" />
       <Button buttonText="blue" color="blue" />
       <Button buttonText="green" color="green" />
