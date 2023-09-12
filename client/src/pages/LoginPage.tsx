03df470087cdafc62a7924049a43c143f80b41c4
sidsidsidsids <sid98816@gmail.com>
2023-09-12 15:32:23 +0900

    Design: 로그인 페이지 CSS 수정 [FRONT-172] - HeaderText 삽입 Related to: issue #172

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import theme from '../style/theme';
+import PageHeaderText from '../components/atoms/PageHeaderText';
 import kakaoLoginButton from '../assets/image/etc/start_kakao_login_large_wide.png';
 
 const LoginPageContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding: 100px;
   width: 100vw;
   height: 100vh;
   background-color: ${theme.colors.mainSkyblue};
+`;
+
+const LoginButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1; /* PageHeaderText와 LoginButton 컴포넌트가 화면을 나눠 가질 수 있도록 flex 속성 추가 */
 `;
 
 const LoginButton = styled.button`
@@ -26,9 +37,12 @@ const LoginPage = () => {
 
   return (
     <LoginPageContainer>
-      <LoginButton onClick={handleButtonClick}>
-        <img src={kakaoLoginButton} alt="kakao" width="600px" height="90px" />
-      </LoginButton>
+      <PageHeaderText content="로그인 및 회원가입" color="dark" />
+      <LoginButtonContainer>
+        <LoginButton onClick={handleButtonClick}>
+          <img src={kakaoLoginButton} alt="kakao" width="600px" height="90px" />
+        </LoginButton>
+      </LoginButtonContainer>
     </LoginPageContainer>
   );
 };
