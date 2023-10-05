4dab86a3ce99b17039c19f0b7491e91ef3f38fa9
sidsidsidsids <sid98816@gmail.com>
2023-10-05 16:38:45 +0900

    Design: whitemodal 반응형 처리 [FRONT-342]

--- client/src/components/atoms/WhiteModal.tsx
+++ client/src/components/atoms/WhiteModal.tsx
@@ -10,9 +10,9 @@ const StyledModal = styled.div<{ height: number }>`
   font-family: 'TmoneyRoundWindExtraBold';
   display: inline-block;
   height: ${(props) => props.height}px;
-  margin: 20px;
-  padding: 50px;
-  border-radius: 25px;
+  margin: 1.25rem;
+  padding: 3.125rem;
+  border-radius: 1.5625rem;
   text-align: center;
   transition: all 0.3s ease-in-out;
   background-color: ${(props) => props.theme.colors.mainWhite};
@@ -21,12 +21,12 @@ const StyledModal = styled.div<{ height: number }>`
 `;
 
 const ModalText = styled.span`
-  font-size: 30px;
+  font-size: 1.875rem;
   color: ${(props) => props.theme.colors.mainBlack};
-  margin-top: 24px;
-  margin-bottom: 24px;
-  margin-right: 34px;
-  margin-left: 34px;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+  margin-right: 2.125rem;
+  margin-left: 2.125rem;
 `;
 
 function WhiteModal({ modalText, height }: ModalProps) {
