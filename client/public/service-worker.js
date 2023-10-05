a3b8bac8245495013190341f41914572f3cb9e36
sidsidsidsids <sid98816@gmail.com>
2023-10-05 14:52:36 +0900

    Feat: service-worker 업데이트 로직 추가 [FRONT-342]

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -6,10 +6,17 @@ self.addEventListener('install', (e) => {
 
 // activate event
 self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] actived', e);
+  // console.log('[Service Worker] actived', e);
 });
 
 // fetch event
 self.addEventListener('fetch', (e) => {
   // console.log(`[Service Worker] fetched resource ${e.request.url}`);
+});
+
+// update event
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
