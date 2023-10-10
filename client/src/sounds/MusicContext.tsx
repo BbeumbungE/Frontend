3d37c08901f43324bd42b43be627c349f531f875
sidsidsidsids <sid98816@gmail.com>
2023-10-10 16:28:07 +0900

    Hotfix: 음량 볼륨 수정

--- client/src/sounds/MusicContext.tsx
+++ client/src/sounds/MusicContext.tsx
@@ -18,15 +18,15 @@ export const BGMProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [playMain, { stop: stopMain }] = useSound(mainBGM, {
-    volume: 0.4,
+    volume: 0.12,
     loop: true,
   });
   const [playStage, { stop: stopStage }] = useSound(stageBGM, {
-    volume: 0.4,
+    volume: 0.12,
     loop: true,
   });
   const [playDraw, { stop: stopDraw }] = useSound(drawBGM, {
-    volume: 0.4,
+    volume: 0.12,
     loop: true,
   });
   const [mainPlaying, setMainPlaying] = useState(false);
